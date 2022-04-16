016eb8bec28501b99e2e6abe4e1d9ac97de025d1
KAZOIIVAS <98618432+KAZOIIVAS@users.noreply.github.com>
2022-04-16 15:36:28 -0600

    el js

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
 console.log("hola mundo");
+
+const inputval=document.getElementById('input-text');
+const  btnAgregar=document.querySelector('#agregar');
+
+
+
+let arrglo= [];
+
+
+
+     //guardamos en el localstorage
+ btnAgregar.addEventListener('click',() => {
+    arrglo.push(inputval.value)
+    localStorage.setItem('lista-tareas',arrglo)
+    inputval.value=''
+    console.log(arrglo)
+    renderelementos(arrglo)
+
+})//
+
+
+
+function renderelementos() {
+    const lista=document.getElementById('tarea');
+    lista.innerHTML='';
+    arrglo.forEach(function(item,i){
+        lista.innerHTML +=  `
+        <li>
+            ${item}
+            <button onclick="borrarElemento(${i})">Borrar</button>
+            <button onclick="Editar(${i})">Editar</button>
+        </li>
+         `
+    })//fin foreach
+    
+}//fin de la funcion render
+function borrarElemento(posicion) {
+    console.log(posicion);
+    arrglo.splice(posicion,1);
+    localStorage.setItem('lista-tareas',arrglo);
+    renderelementos();
+    console.log(arrglo);
+    
+}
+
+function Editar(i){
+    inputval.value=arrglo[i];
+    let val=i;
+ 
+    btnAgregar.style.display="none";
+    const newbtn=document.getElementById("edit");
+    newbtn.innerHTML += `
+    <button  id="Actualizar" onclick="actualizar(${val})">Guardar</button>
+   `
+ 
+  
+    console.log("yes");
+
+}
+function actualizar(j) {
+       arrglo[j]=inputval.value;
+        localStorage.setItem('lista-tareas',arrglo);
+        renderelementos();
+        inputval.value='';
+
+         let otrobtn=document.getElementById('Actualizar');
+         otrobtn.remove();
+         btnAgregar.style.display="initial";
+         console.log(arrglo);
+   }
+
